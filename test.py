2e3bbd8e5351106765161cdf11fb9b062a2a6eeb
Neelesh Sharma <neelesh.sharma.20@hotmail.com>
2023-04-20 17:38:12 +0530

    test 1.8

--- test.py
+++ test.py
@@ -183,3 +183,21 @@ def servo():
     SetAngle(30)
     pwm.stop()
     GPIO.cleanup()
+
+def ir():
+    import RPi.GPIO as GPIO
+    import time
+
+    sensor = 16
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(sensor,GPIO.IN)
+
+    try: 
+        while True:
+            if GPIO.input(sensor):
+                print("Object Detected")
+                while GPIO.input(sensor):
+                    time.sleep(0.4)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
